64f23d1b9b370c15af1c913f05e858b8597b2497
ruijpedro <rjpedro@gmail.com>
2026-06-29 09:53:54 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './',
   plugins: [react()],
-  base: '/Familia-Rolim-Pedro/'
+  build: { outDir: 'dist' }
 })
